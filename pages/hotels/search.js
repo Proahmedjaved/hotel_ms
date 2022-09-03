035e168f653a9ad0184d91f41db5ed09b9ebe662
noorsiddiqui-prog <noorsiddiqui048@gmail.com>
2022-09-03 15:51:31 +0500

    modified 3/9

--- pages/hotels/search.js
+++ pages/hotels/search.js
@@ -1,7 +1,17 @@
-export default function search_hotel(){
-    return(
-        <div>
-            <h1>Search Online Hotel</h1>
-        </div>
-    )
-}
+// import React from 'react'
+// import Login from "../../components/cus_hotel/login"
+// // import Signup from "../../components/cus_hotel/Signup"
+// import HeaderItem from "../../components/cus_hotel/HeaderItem"
+// import Footer from "../../components/cus_hotel/Footer"
+
+// export default function search_hotel(){
+//     return(
+//         <>
+//         <HeaderItem />
+        
+//             <Login />
+//             {/* <Signup /> */}
+//         <Footer />
+//         </>
+//     )
+// }
